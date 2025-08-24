09c466adcc0338e20d71cb6f309423ae2f5315ec
AIChoubeyX <ashutoshchoubey2004@gmail.com>
2025-08-24 20:18:51 +0530

    functions part one

---
+++ src/function/builtin.java
@@ -0,0 +1,18 @@
+package src.function;
+
+public class builtin {
+    public static void main(String[] args){
+        // math.wale
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.cbrt(16));
+        System.out.println(Math.abs(-5));  // absolute value
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.floor(163.8987)); // greatest integer function
+        System.out.println(Math.ceil(163.8987));  // smallest integer function
+        System.out.println(Math.round(163.8987)); // round function
+        System.out.println(Math.pow(2, 3)); // power function
+        System.out.println(Math.random()); // random function
+
+    }
+    
+}
---
+++ src/function/syntax.java
@@ -0,0 +1,13 @@
+package src.function;
+
+public class syntax {
+    public static void choubey (){
+        System.out.println("This is choubey function");
+    }
+    public static void main(String[] args){
+        choubey();
+        System.out.println("This is main function");
+
+    }
+    
+}
